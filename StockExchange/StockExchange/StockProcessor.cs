40d649f21c232515b252feb6cd4271ecdcf04cba
arjunpunnappurath <40994090+arjunpunnappurath@users.noreply.github.com>
2019-04-11 22:40:06 +0530

    Implemented SRP
    Further simplified the function logics

--- StockExchange/StockExchange/StockProcessor.cs
+++ StockExchange/StockExchange/StockProcessor.cs
@@ -12,53 +12,105 @@ namespace StockExchange
     public class StockProcessor 
     {
         Dictionary<string, List<Stock>> stockState = new Dictionary<string, List<Stock>>();
-        public  List<Stock> ProcessstockOrderList(List<Stock> stockOrderList)
+        public List<Stock> ProcessstockOrderList(List<Stock> stockOrderList)
         {
-            List<Stock> existingOrders = new List<Stock>();
+           
             foreach (var stock in stockOrderList)
             {
-                if (stockState.TryGetValue(stock.Company,  out existingOrders))
-                {
-                    if (stock.Side == StockSide.Buy)
-                        existingOrders.Add(stock);
-                    else 
-                    {
-                        foreach (var item in existingOrders)
-                        {
-                            int buffQty;
-                            if(stock.RemQuantity > item.RemQuantity && item.Side == StockSide.Buy)
-                            {
-                                buffQty = stock.RemQuantity - item.RemQuantity;
-                                stock.UpdateRemainingQuantity(buffQty);
-                                item.UpdateRemainingQuantity(0);
-                            }
-                            else if (item.RemQuantity > stock.RemQuantity && item.Side == StockSide.Buy)
-                            {
-                                buffQty = item.RemQuantity - stock.RemQuantity;
-                                item.UpdateRemainingQuantity(buffQty);
-                                stock.UpdateRemainingQuantity(0);
-                                break;
-                            }
-                            else if(item.RemQuantity == stock.RemQuantity && item.Side == StockSide.Buy)
-                            {
-                                buffQty = Math.Abs(stock.RemQuantity - item.RemQuantity);
-                                stock.UpdateRemainingQuantity(buffQty);
-                                item.UpdateRemainingQuantity(buffQty);
-                                break;
-                            }
-                         
-                        }
-                    }
-                }
-                else
+                var existingOrders = GetExisingOrdersForCompany(stock);
+                if(existingOrders == null)
                 {
                     List<Stock> stocks = new List<Stock>();
                     stocks.Add(stock);
                     stockState.Add(stock.Company, stocks);
                 }
+                else if(existingOrders != null && stock.Side == StockSide.Sell)
+                {
+                    processSellingIn(existingOrders, stock);
+                }
             }
             return stockOrderList;
         }
 
+        private List<Stock> GetExisingOrdersForCompany(Stock stock)
+        {
+            List<Stock> existingOrders = new List<Stock>();
+            if (stockState.TryGetValue(stock.Company, out existingOrders))
+            {
+                if (stock.Side == StockSide.Buy)
+                    existingOrders.Add(stock);
+                return existingOrders;
+            }
+            return null;
+        }
+
+        private void processSellingIn(List<Stock> existingOrders, Stock stock)
+        {
+            foreach (var item in existingOrders)
+            {
+                //int buffQty;
+
+                if (stock.RemQuantity > item.RemQuantity && item.Side == StockSide.Buy)
+                {
+                    //buffQty = stock.RemQuantity - item.RemQuantity;
+                    //stock.UpdateRemainingQuantity(buffQty);
+                    //item.UpdateRemainingQuantity(0);
+                    SellAndCloseCurrentOrder(item, stock);
+                }
+                else if (item.RemQuantity > stock.RemQuantity && item.Side == StockSide.Buy)
+                {
+                    //buffQty = item.RemQuantity - stock.RemQuantity;
+                    //item.UpdateRemainingQuantity(buffQty);
+                    //stock.UpdateRemainingQuantity(0);
+                    bool val = SellAndCloseExistingOrder(item, stock);
+                    break;
+                }
+                else if (item.RemQuantity == stock.RemQuantity && item.Side == StockSide.Buy)
+                {
+                    //buffQty = Math.Abs(stock.RemQuantity - item.RemQuantity);
+                    //stock.UpdateRemainingQuantity(buffQty);
+                    //item.UpdateRemainingQuantity(buffQty);
+                    bool val = SellandCloseBothItems(item, stock);
+                    break;
+                }
+            }
+        }
+
+        private bool SellandCloseBothItems(Stock item, Stock stock)
+        {
+            try
+            {
+                int buffQty = Math.Abs(stock.RemQuantity - item.RemQuantity);
+                stock.UpdateRemainingQuantity(buffQty);
+                item.UpdateRemainingQuantity(buffQty);
+                return true;
+            }
+           catch
+            {
+                return false;
+            }
+        }
+
+        private void SellAndCloseCurrentOrder(Stock item, Stock stock)
+        {
+            int buffQty = stock.RemQuantity - item.RemQuantity;
+            stock.UpdateRemainingQuantity(buffQty);
+            item.UpdateRemainingQuantity(0);
+        }
+
+        private bool SellAndCloseExistingOrder(Stock item, Stock stock)
+        {
+            try
+            {
+                int buffQty = item.RemQuantity - stock.RemQuantity;
+                item.UpdateRemainingQuantity(buffQty);
+                stock.UpdateRemainingQuantity(0);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
     }
 }
